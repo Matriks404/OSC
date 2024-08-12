cd06f56b929f96d0ed8d5fd72abbf60fae30462c
Marcin Kralka <marcink96@gmail.com>
2024-08-12 02:42:15 +0200

    Attempt to fix SHTMLDG not being a submodule (1/2?)

--- SHTMLDG/make_example_docs.Unix.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env sh
-
-echo Generating an example documentation site in \'site/\' directory.
-./program.py examples/example_documentation site
-echo Done.
--- SHTMLDG/make_example_docs.Windows.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-
-echo Generating an example documentation site in 'site/' directory.
-python3 program.py examples/example_documentation site
-echo Done.
