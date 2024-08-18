f86c18762b88e1830b88307db070a6443d3d8379
Marcin Kralka <marcink96@gmail.com>
2024-08-18 18:44:48 +0200

    Improved compiler script.

--- Compile.Unix.sh
+++ Compile.Unix.sh
@@ -1,4 +1,8 @@
+echo === OKSB Compiler Version 1.0 ===
+echo
 echo Compiling OSKB...
 cd SHTMLDG
 ./program.py ../src ../site
 echo Done.
+echo
+read -n1 -r -p "Press any key to continue..."
--- Compile.Windows.bat
+++ Compile.Windows.bat
@@ -1,5 +1,9 @@
 @echo off
+echo === OKSB Compiler Version 1.0 ===
+echo.
 echo Compiling OSKB...
 cd SHTMLDG
 python3 .\program.py ..\src ..\site
 echo Done.
+echo.
+pause
